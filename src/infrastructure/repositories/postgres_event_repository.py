8b573e2acbd88185c078c193d23316fa8357608c
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2026-05-13 20:10:47 +0200

    correct mistakes

--- src/infrastructure/repositories/postgres_event_repository.py
+++ src/infrastructure/repositories/postgres_event_repository.py
@@ -38,7 +38,7 @@ class PostgresEventRepository(IEventRepository):
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8,$9, $10, $11, $12, $13, $14)
         """
         try:
-            await self.pool.execute(
+            await self._pool.execute(
                 query,
                 event.id, event.title, event.description, event.location,
                 event.genre, event.event_type, event.start_at, event.end_at,
@@ -50,7 +50,7 @@ class PostgresEventRepository(IEventRepository):
 
     async def get_by_id(self, event_id: UUID) -> Event:
         query = "SELECT * FROM events WHERE id = $1"
-        row = await self.pool.fetchrow(query, event_id)
+        row = await self._pool.fetchrow(query, event_id)
 
         if not row:
             return None
@@ -58,19 +58,19 @@ class PostgresEventRepository(IEventRepository):
 
     async def get_all(self, limit: int = 100) -> Sequence[Event]:
         query = "SELECT * FROM events ORDER BY start_at DESC"
-        rows = await self.pool.fetch(query, limit)
+        rows = await self._pool.fetch(query, limit)
         return [self._map_to_domain(row) for row in rows]
 
     async def get_by_event_type(self, event_type: str) -> Sequence[Event]:
         query = "SELECT * FROM events WHERE event_type = $1 ORDER BY start_at DESC"
-        rows = await self.pool.fetch(query, event_type)
+        rows = await self._pool.fetch(query, event_type)
         return [self._map_to_domain(row) for row in rows]
 
     async def get_by_start_at(self, start_at: datetime) -> Sequence[Event]:
         query = "SELECT * FROM events WHERE start_at = $1 ORDER BY start_at DESC"
-        rows = await self.pool.fetch(query, start_at)
+        rows = await self._pool.fetch(query, start_at)
         return [self._map_to_domain(row) for row in rows]
 
     async def delete(self, event_id: UUID) -> None:
         query = "DELETE FROM events WHERE id = $1"
-        await self.pool.execute(query, event_id)
+        await self._pool.execute(query, event_id)
