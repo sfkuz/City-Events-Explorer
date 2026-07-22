486ed152ad2a30bf3491463f2cf4c21e875fc2f2
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2026-07-22 22:00:23 +0200

    update library becouse the previous library was not reliable enough

--- src/infrastructure/scraping/fetchers/static.py
+++ src/infrastructure/scraping/fetchers/static.py
@@ -1,5 +1,6 @@
 import httpx
 import logging
+from fake_useragent import UserAgent
 from application.scraping.ports import IFetcher
 from application.scraping.retry import with_retry
 
@@ -8,16 +9,20 @@ logger = logging.getLogger(__name__)
 
 class StaticFetcher(IFetcher):
     def __init__(self):
-        self.default_headers = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
-        }
+        self.ua = UserAgent(os='windows', browsers=['chrome', 'firefox', 'opera', 'safari'])
+
     @with_retry()
     async def fetch_html(self, url: str, headers: dict | None = None) -> str:
-        req_headers = self.default_headers.copy()
+        req_headers = {
+            "User-Agent": self.ua.random,
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
+        }
+
         if headers:
             req_headers.update(headers)
 
-        logger.info(f"Fetching URL: {url}")
+        logger.info(f"Fetching URL: {url} | UA: {req_headers['User-Agent']}")
 
         async with httpx.AsyncClient(timeout=15.0, follow_redirects=True) as client:
             response = await client.get(url, headers=req_headers)
