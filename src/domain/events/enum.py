449175ce4a6e16e7b9dec8be10a7bf21bd5106c5
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2026-07-24 18:07:18 +0200

    add constant lists

---
+++ src/domain/events/enum.py
@@ -0,0 +1,10 @@
+AVAILABLE_GENRES = [
+    'jazz', 'pop', 'rock/punk', 'muzyka poważna',
+    'muzyka elektroniczna', 'muzyka filmowa',
+    'muzyka alternatywna', 'hip-hop',
+    'festiwal muzyczny', 'blues/soul'
+]
+
+AVAILABLE_TYPES = [
+    'koncerty', 'imprezy rozrywkowe'
+]
