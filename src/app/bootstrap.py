b44284e129eb49f01ee231b855d6a32852d87c5c
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2026-08-17 22:16:52 +0200

    update bootstrap

--- src/app/bootstrap.py
+++ src/app/bootstrap.py
@@ -1,12 +1,28 @@
 from __future__ import annotations
 
 import logging
-import asyncpg
 from contextlib import AsyncExitStack
 from dataclasses import dataclass
 
-from infrastructure.config import Settings, load_settings
+from aiogram import Bot, Dispatcher
+
+from infrastructure.config import Settings
 from infrastructure.db.pool import create_db_pool, close_db_pool
+
+from infrastructure.repositories.postgres_feed_repository import PostgresFeedRepository
+from infrastructure.repositories.postgres_event_repository import PostgresEventRepository
+from infrastructure.repositories.postgres_event_listings_repository import PostgresEventListingsRepository
+
+from infrastructure.scraping.sources.trojmiasto.scraper import TrojmiastoScraper
+from infrastructure.scraping.fetchers.static import StaticFetcher
+from infrastructure.scraping.registry import ScraperRegistry
+
+from application.events.service import EventService
+from application.scraping.service import ScraperService
+from application.scraping.normalize import NormalizationService
+from application.cron_service import CronService
+
+from app.tg_bot.handlers.router import main_router
 
 logger = logging.getLogger(__name__)
 
@@ -14,17 +30,49 @@ logger = logging.getLogger(__name__)
 @dataclass(slots=True, kw_only=True)
 class Application:
     settings: Settings
-    db_pool: asyncpg.Pool
+    bot: Bot
+    dp: Dispatcher
+    cron_service: CronService
 
 
-async def bootstrap_application(stack: AsyncExitStack) -> Application:
-    settings = load_settings()
-    logger.info("Settings loaded successfully")
+async def bootstrap_application(settings: Settings,stack: AsyncExitStack) -> Application:
+    logger.info("Bootstrapping application dependencies...")
+
     db_pool = await create_db_pool(settings)
     stack.push_async_callback(close_db_pool, db_pool)
-    logger.info("Database pool initialized")
+
+    event_repo = PostgresEventRepository(db_pool)
+    feed_repo = PostgresFeedRepository(db_pool)
+    listings_repo = PostgresEventListingsRepository(db_pool)
+
+    fetcher = StaticFetcher()
+    trojmiasto_scraper = TrojmiastoScraper(fetcher)
+
+    registry = ScraperRegistry()
+    registry.register("trojmiasto", trojmiasto_scraper)
+
+    event_service = EventService(event_repo)
+    normalization_service = NormalizationService(listings_repo, event_repo)
+
+    scraper_service = ScraperService(
+        feed_repo = feed_repo,
+        listings_repo = listings_repo,
+        registry = registry,
+        normalization_service = normalization_service
+    )
+
+    cron_service = CronService(scraper_service)
+    stack.callback(cron_service.stop)
+
+    bot = Bot(token=settings.tg_bot_token)
+
+    dp = Dispatcher()
+    dp["event_service"] = event_service
+    dp.include_router(main_router)
 
     return Application(
         settings=settings,
-        db_pool=db_pool,
+        bot=bot,
+        dp=dp,
+        cron_service=cron_service,
     )
