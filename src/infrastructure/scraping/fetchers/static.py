4a17885c1e464554507bba8ea8d2c81cb96f2bf6
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2026-09-01 18:13:48 +0200

    delete library fake useragent. replace to standart user agent

--- src/infrastructure/scraping/fetchers/static.py
+++ src/infrastructure/scraping/fetchers/static.py
@@ -1,20 +1,26 @@
 import httpx
 import logging
-from fake_useragent import UserAgent
+import random
 from application.scraping.ports import IFetcher
 from application.scraping.retry import with_retry
 
 logger = logging.getLogger(__name__)
 
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
+]
+
 
 class StaticFetcher(IFetcher):
     def __init__(self):
-        self.ua = UserAgent(os='windows', browsers=['chrome', 'firefox', 'opera', 'safari'])
+        pass
 
     @with_retry()
     async def fetch_html(self, url: str, headers: dict | None = None) -> str:
         req_headers = {
-            "User-Agent": self.ua.random,
+            "User-Agent": random.choice(USER_AGENTS),
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
             "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
         }
